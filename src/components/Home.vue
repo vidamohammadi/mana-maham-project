27996254282eeeee5efef0f829d3090e7ea67215
vida <m.vidamohammadi@gmail.com>
2025-09-17 00:26:24 -0800

    add: create navbar and add to home page and use mdi for icon

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,15 +1,17 @@
 <template>
 <v-container >
+  <Navbar />
   <Header />
 </v-container>
 </template>
 
 <script>
 import Header from "./Header.vue";
+import Navbar from "./Navbar.vue";
 
 export default {
   name: "Home",
-  components: {Header}
+  components: {Navbar, Header}
 }
 </script>
 
---
+++ src/components/Navbar.vue
@@ -0,0 +1,27 @@
+<template>
+<v-row class="d-flex justify-space-between">
+<v-col cols="6">
+<v-icon color="orange" size="40">
+  mdi-book-open-page-variant
+</v-icon>
+</v-col>
+<v-col cols="6" dir="rtl">
+  <v-btn class="bg-orange" color="white" variant="outlined" @click="logOut">Log out</v-btn>
+</v-col>
+  <v-divider />
+</v-row>
+</template>
+
+<script setup>
+import { useRouter } from "vue-router"
+import '@mdi/font/css/materialdesignicons.css'
+
+const router = useRouter()
+const logOut = () =>{
+   router.push({ name: "login" })
+}
+</script>
+
+<style scoped>
+
+</style>
