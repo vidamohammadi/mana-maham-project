8b79ba0e4914ed76e5b785e8cec93c8d533799d7
vida <m.vidamohammadi@gmail.com>
2025-09-16 17:55:32 -0800

    add: pinia as state management for handle data

--- src/main.js
+++ src/main.js
@@ -2,10 +2,13 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify'
 import router from './router'
+import {createPinia} from "pinia";
 
 
 const app = createApp(App)
+const pinia = createPinia()
 
+app.use(pinia)
 app.use(vuetify)
 app.use(router)
 app.mount('#app')
