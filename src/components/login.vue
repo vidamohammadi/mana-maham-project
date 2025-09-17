66c61d81b513495be72c624597d4cc7fb01144d5
vida <m.vidamohammadi@gmail.com>
2025-09-16 18:39:18 -0800

    add: create login form | components

--- src/components/login.vue
+++ src/components/login.vue
@@ -1,13 +1,34 @@
 <template>
+  <v-container fluid class="fill-height">
+    <v-row class="align-center justify-center">
+      <v-sheet class="mx-auto border border-success border-opacity-100 pa-5 " width="400">
+        <v-form>
+          <v-text-field
+              v-model="userName"
+              variant="outlined"
+              label="User name"
+          ></v-text-field>
+          <v-text-field
+              v-model="password"
+              variant="outlined"
+              label="Password"
+          ></v-text-field>
+          <v-btn class="mt-2 bg-success" type="submit" block>Login</v-btn>
+        </v-form>
+      </v-sheet>
+    </v-row>
 
-  <v-btn class="bg-red">test</v-btn>
 
- </template>
 
-<script>
-export default {
-  name: "login"
-}
+   </v-container>
+
+</template>
+<script setup>
+import {ref} from "vue"
+
+const userName = ref('')
+const password = ref('')
+
 </script>
 
 <style scoped>
