28452cd67d7675066e467ce398d1174fefd39168
vida <m.vidamohammadi@gmail.com>
2025-09-17 00:12:19 -0800

    add: create header and add to home page

---
+++ src/components/Header.vue
@@ -0,0 +1,33 @@
+<template>
+<v-row>
+  <v-col cols="12" md="6" class="mt-3">
+    <v-img :src="headerStore.header.imageUrl" />
+   </v-col>
+  <v-col cols="12" md="6">
+    <h1 class="mb-5">
+      {{headerStore.header.title}}
+    </h1>
+    <p>
+      {{headerStore.header.description}}
+    </p>
+  </v-col>
+</v-row>
+</template>
+
+<script setup>
+import { onMounted } from "vue";
+import { useHeaderStore } from "../stores/header.js";
+import header from "../data/header.json";
+
+const headerStore = useHeaderStore();
+
+onMounted(() => {
+  headerStore.setHeader(header);
+  console.log("Section Items:", headerStore.header);
+});
+</script>
+
+
+<style scoped>
+
+</style>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,12 +1,15 @@
 <template>
-<div>
-  home
-</div>
+<v-container >
+  <Header />
+</v-container>
 </template>
 
 <script>
+import Header from "./Header.vue";
+
 export default {
-  name: "Home"
+  name: "Home",
+  components: {Header}
 }
 </script>
 
